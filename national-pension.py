652a167d569ad53c21588d18e08316d48efdead9
SoonwookKweon <sukwon93@gmail.com>
2025-12-04 14:10:38 +0900

    URL modified
    Updated the file to include necessary imports and changed the data source for pension data.

--- national-pension.py
+++ national-pension.py
@@ -73,7 +73,7 @@ class PensionData():
 
 @st.cache_data # 미리 캐시해놓아서 전처리를 다시 하지 않아도 되도록!
 def read_pensiondata():
-    data = PensionData('./data/국민연금공단_국민연금 가입 사업장 내역_20251124.csv')
+    data = PensionData('https://www.dropbox.com/s/nxeo1tziv05ejz7/national-pension.csv?dl=1')
     return data
 
 data = read_pensiondata()
@@ -152,3 +152,4 @@ if data and company_name:
     else:
 
         st.subheader('검색결과가 없습니다')
+
